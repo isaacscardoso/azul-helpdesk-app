090e24116d1c7a1684411b9b4b76c5f90392aac0
Isaac Cardoso Silva <mukanshinbluedev@outlook.com>
2022-01-25 16:23:00 -0300

    (backend) Project created

---
+++ azul-helpdesk-app-backend/src/main/java/com/azul/isaacscardoso/azulhelpdeskappbackend/AzulHelpdeskAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.azul.isaacscardoso.azulhelpdeskappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzulHelpdeskAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzulHelpdeskAppBackendApplication.class, args);
+	}
+
+}
---
+++ azul-helpdesk-app-backend/src/test/java/com/azul/isaacscardoso/azulhelpdeskappbackend/AzulHelpdeskAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azul.isaacscardoso.azulhelpdeskappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzulHelpdeskAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
