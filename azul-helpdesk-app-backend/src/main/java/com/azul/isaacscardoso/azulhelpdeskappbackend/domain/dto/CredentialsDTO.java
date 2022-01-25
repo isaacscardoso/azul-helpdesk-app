385131adbcadcd30927d9c7ff113d5a840a3c508
Isaac Cardoso Silva <mukanshinbluedev@outlook.com>
2022-01-25 16:38:43 -0300

    (backend) Creating CredentialsDTO

---
+++ azul-helpdesk-app-backend/src/main/java/com/azul/isaacscardoso/azulhelpdeskappbackend/domain/dto/CredentialsDTO.java
@@ -0,0 +1,11 @@
+package com.azul.isaacscardoso.azulhelpdeskappbackend.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CredentialsDTO {
+    private String email;
+    private String password;
+}
