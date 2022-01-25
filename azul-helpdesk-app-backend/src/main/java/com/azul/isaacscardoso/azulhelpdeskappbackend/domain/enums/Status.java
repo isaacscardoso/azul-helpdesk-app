d92ba471e247562dbb7848dbaf7f0fec239cecc7
Isaac Cardoso Silva <mukanshinbluedev@outlook.com>
2022-01-25 16:57:14 -0300

    (backend) Creating 'enum' of status

---
+++ azul-helpdesk-app-backend/src/main/java/com/azul/isaacscardoso/azulhelpdeskappbackend/domain/enums/Status.java
@@ -0,0 +1,31 @@
+package com.azul.isaacscardoso.azulhelpdeskappbackend.domain.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Status {
+    OPEN(0, "OPEN"),
+    IN_PROGRESS(1, "IN_PROGRESS"),
+    CLOSED(2, "HIGH");
+
+    private final Integer code;
+    private final String description;
+
+    Status(Integer code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public static Status toEnum(Integer code) {
+        if (code == null) {
+            return null;
+        } else {
+            for (Status p : Status.values()) {
+                if (code.equals(p.getCode())) {
+                    return p;
+                }
+            }
+        }
+        throw new IllegalArgumentException("Esta Prioridade está inválida!");
+    }
+}
